83c1aa1ea31db217fa93bbf3d93a125fd9cb1446
rzinhub <rafael.alves.oliveira03@escola.pr.gov.br>
2025-10-23 10:15:24 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 alert("Este projeto contém conteúdo sensível!!!")
+
+const avanca = document.querySelectorAll('.btn.proximo')
+console.log (avanca)
+
+avanca.forEach (button => {
+    button.addEventListener('click',function () {
+const atual = document.querySelector(',.ativo')
+    })
+})
+
+
+
+
+
+
+
+
+
+
