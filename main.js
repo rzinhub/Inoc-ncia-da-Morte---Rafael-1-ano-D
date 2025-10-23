137aac1f0ec58e0edf9ac0626dd06d046af46055
rzinhub <rafael.alves.oliveira03@escola.pr.gov.br>
2025-10-23 10:25:33 -0300

    .

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ console.log (avanca)
 avanca.forEach (button => {
     button.addEventListener('click',function () {
 const atual = document.querySelector(',.ativo')
-const proximoPasso = 'passo.' + this.getAttribute('data.p')
+const proximoPasso = 'passo.' + this.getAttribute('data-proximo')
+atual.classList. remove ('ativo')
+document.getElementById(proximoPasso) . classList.add('ativo')
     })
 })
 
